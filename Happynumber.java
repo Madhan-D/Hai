15c13ad131ffac4631b47502a27abf42c50bf5c9
Madhan-D <madhan001995@gmail.com>
2016-09-08 15:46:51 +0530

    Add files via upload

---
+++ Happynumber.java
@@ -0,0 +1,49 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+import com.sun.xml.internal.ws.util.StringUtils;
+import java.util.Scanner;
+import java.util.Arrays;
+/**
+ *
+ * @author Student
+ */
+public class Happynumber {
+
+
+  	public static void main(String[] args) {
+
+Scanner s= new Scanner(System.in);
+
+
+int n=s.nextInt();
+if(hpy(n)==1)
+                System.out.println("happy");
+else
+                System.out.println("not happy");
+
+
+    } 
+        public static int hpy(int n)
+        {
+            int s=0;
+            while (n!=0)
+            {
+                s+=Math.pow(n%10,2);
+                n=n/10;
+            }
+            if(s<10)
+                return s;
+            else
+            {
+                n=s;
+                return hpy(n);
+            }
+        }
+}
+
+
+
